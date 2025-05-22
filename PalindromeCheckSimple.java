a104026aebe80a0732bb19984239ab59a8695365
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-22 23:40:01 +0530

    palindrome

---
+++ PalindromeCheckSimple.java
@@ -0,0 +1,17 @@
+
+    public class PalindromeCheckSimple {
+    public static void main(String[] args) {
+        String str = "madam";
+        boolean isPalindrome = true;
+
+        for (int i = 0; i < str.length() / 2; i++) {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        System.out.println(isPalindrome ? "Palindrome" : "Not a Palindrome");
+    }
+}
+
