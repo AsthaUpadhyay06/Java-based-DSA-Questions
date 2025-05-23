77f1a839cda829cc7e33fe05f4d08cf0d5dd4d83
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-23 23:42:31 +0530

    ReverseString

---
+++ ReverseStringLoop.java
@@ -0,0 +1,10 @@
+public class ReverseStringLoop {
+    public static void main(String[] args) {
+        String original = "Hello, astha!";
+        String reversed = "";
+        for (int i = original.length() - 1; i >= 0; i--) {
+            reversed += original.charAt(i);
+        }
+        System.out.println("Reversed string: " + reversed);
+    }
+}
