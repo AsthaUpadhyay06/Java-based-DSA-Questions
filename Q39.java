1216be05c1cf52a559bc0e6804da744006f13068
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-26 23:39:47 +0530

    Matrix Multiplication

---
+++ Q39.java
@@ -0,0 +1,51 @@
+public class Q39{
+    
+    public static void spiralOrder(int[][] matrix) {
+        if (matrix == null || matrix.length == 0) return;
+
+        int top = 0, bottom = matrix.length - 1;
+        int left = 0, right = matrix[0].length - 1;
+
+        while (top <= bottom && left <= right) {
+            // Traverse from left to right
+            for (int i = left; i <= right; i++) {
+                System.out.print(matrix[top][i] + " ");
+            }
+            top++;
+
+            // Traverse from top to bottom
+            for (int i = top; i <= bottom; i++) {
+                System.out.print(matrix[i][right] + " ");
+            }
+            right--;
+
+            // Traverse from right to left (if still within bounds)
+            if (top <= bottom) {
+                for (int i = right; i >= left; i--) {
+                    System.out.print(matrix[bottom][i] + " ");
+                }
+                bottom--;
+            }
+
+            // Traverse from bottom to top (if still within bounds)
+            if (left <= right) {
+                for (int i = bottom; i >= top; i--) {
+                    System.out.print(matrix[i][left] + " ");
+                }
+                left++;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        System.out.println("Spiral Order Traversal:");
+        spiralOrder(matrix);
+    }
+}
