5443a1eb12dcc4c2c0dcfe4cfc2636147f013852
Astha Upadhyay <asthaupadhyay046@gmail.com>
2025-05-27 23:59:52 +0530

    Snake pattern

---
+++ Q43.java
@@ -0,0 +1,28 @@
+public class Q43 {
+    public static void main(String[] args) {
+        int[][] matrix = {
+            {1, 2, 3, 4},
+            {5, 6, 7, 8},
+            {9, 10, 11, 12},
+            {13, 14, 15, 16}
+        };
+
+        int rows = matrix.length;
+        int cols = matrix[0].length;
+
+        System.out.println("Snake Pattern:");
+        for (int i = 0; i < rows; i++) {
+            if (i % 2 == 0) {
+                // Left to right
+                for (int j = 0; j < cols; j++) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            } else {
+                // Right to left
+                for (int j = cols - 1; j >= 0; j--) {
+                    System.out.print(matrix[i][j] + " ");
+                }
+            }
+        }
+    }
+}
